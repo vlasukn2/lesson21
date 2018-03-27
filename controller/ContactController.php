4ccd01abe2f255ead93062ea5f0e83003c5aba31
n.vlasuk <n.vlasuk@lucky-labs.com>
2018-03-27 20:28:36 +0300

    phpmailer

--- controller/ContactController.php
+++ controller/ContactController.php
@@ -11,6 +11,7 @@ namespace controller;
 
 use lib\Controller;
 use model\Message;
+use PHPMailer\PHPMailer\PHPMailer;
 
 class ContactController extends  Controller
 {
@@ -31,7 +32,75 @@ class ContactController extends  Controller
 
         $this->model->addMessage([$name, $email, $message]);
         $this->setFlash("Message added!");
-        $this->redirect('/21/web/contact');
+
+
+        if ($this->sendEmail($name, $email, $message)) {
+            $this->redirect('/21/web/contact');
+        }
+
+
+    }
+
+    private function sendEmail($name, $email, $message)
+    {
+        $name    = htmlspecialchars($name);
+        $email   = htmlspecialchars($email);
+        $message = htmlspecialchars($message);
+
+
+        $mailer = new PHPMailer(true);
+
+        try {
+            $mailer->isSMTP();
+
+            $mailer->SMTPAuth = true;
+            $mailer->SMTPDebug = 0;
+
+
+            $mailer->Host = 'smtp.gmail.com';
+
+            $mailer->SMTPSecure = 'ssl';
+            $mailer->Port = 465;
+
+            $mailer->SMTPSecure = 'tls';
+            $mailer->Port = 587;
+
+            $u = 'phpacademy0711@gmail.com';
+            $p = 'phpacademy0711+';
+
+            $mailer->Username = $u;
+            $mailer->Password = $p;
+
+            $mailer->SMTPOptions = array(
+                'ssl' => array(
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed' => true
+                )
+            );
+
+
+            $mailer->setFrom('vlasukn@mail.ru', 'nick');
+            $mailer->addReplyTo('replyto@example.com', 'First Last');
+
+            $mailer->addAddress('phpacademy0711@gmail.com', 'php-class');
+
+            $mailer->Subject = "New Contact Message";
+            $mailer->Body = <<<BODY
+Name: $name
+Email: $email
+Message: $message
+BODY;
+;
+            $mailer->AltBody = 'This is a plain-text message body';
+
+            return $mailer->send();
+
+
+        } catch (\Exception $e) {
+
+            $this->setFlash($e->getMessage());
+        }
     }
 
 }
